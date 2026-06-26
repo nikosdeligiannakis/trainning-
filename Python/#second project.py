c85a39c21ab3546360c24272dda7444b6892b2fb
nikosdeligiannakis <156413563+nikosdeligiannakis@users.noreply.github.com>
2026-06-26 17:38:48 +0300

    Add files via upload

---
+++ Python/#second project.py
@@ -0,0 +1,5 @@
+#second project
+print("thank you for selected our apps for your day to day actions")
+fname = input("what is your pet name? ")
+lname = input("what is your city? ")
+print(f"your new twitter handle and bio @cyber{fname} from {lname}")
